cb8f0ce49559d136d654f2d0fd178f9578a869b1
Sovrano28 <Adeyemiayokanmi47@gmail.com>
2022-11-19 19:48:26 +0100

    added the nacbar toggler
    now the navbar does't show automatically on smaller device. also added the aos.js function but it still not smooth scrolling.

---
+++ scripts/script.js
@@ -0,0 +1,42 @@
+(function () {
+  "use strict";
+
+  const select = (el, all = false) => {
+    el = el.trim()
+    if (all) {
+      return [...document.querySelectorAll(el)]
+    } else {
+      return document.querySelector(el)
+    }
+  }
+
+  const on = (type, el, listener, all = false) => {
+    let selectEl = select(el, all)
+    if (selectEl) {
+      if (all) {
+        selectEl.forEach(e => e.addEventListener(type, listener))
+      } else {
+        selectEl.addEventListener(type, listener)
+      }
+    }
+  }
+
+  on('click', '.mobile-nav-toggle', function (e) {
+    select('body').classList.toggle('mobile-nav-active');
+    this.classList.toggle('bi-list');
+    this.classList.toggle('bi-x');
+  });
+
+  // aos smooth scroll function
+  window.addEventListener('load', () => {
+    AOS.init({
+      duration: 1000,
+      easing: 'ease-in-out',
+      once: true,
+      mirror: false
+    })
+  });
+
+  new PureCounter();
+
+})();

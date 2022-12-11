400d4614931cd7490766a4a7225ed1d077a51465
Ayokanmi Adeyemi <101461930+Sovrano28@users.noreply.github.com>
2022-12-11 06:16:13 +0100

    corrected an error by removing an un-needed div tag in index.html
    also made some correction to ul.my-stack although I'm still coming back to revamp the whole my skill section

--- scripts/script.js
+++ scripts/script.js
@@ -23,6 +23,7 @@
     }
   };
 
+  // mobile-nav icon toggling.
   on('click', '.mobile-nav-toggle', function (e) {
     select('body').classList.toggle('mobile-nav-active');
     this.classList.toggle('bi-list');
